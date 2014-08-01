c771c0d2c227db51743c55de9e9b53986c22244e
Jeffrey Santi <jeffreysanti@gmail.com>
2014-08-01 14:41:56 -0400

    Build System Almost Done!!

--- Server/buildscript.sh
+++ Server/buildscript.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 GCCROOT=""
+FLAG=""
 
 if [ "$1" == "" ]; then
 	GCCROOT="/bin/"
@@ -10,48 +11,31 @@ else
 	echo "Using gcc installation (in $GCCROOT)"
 fi
 
+if [ "$2" == "32" ]; then
+	FLAG=" -m32 "
+	echo "Building 32-Bit Version"
+else
+	FLAG=" -m64 "
+	echo "Building 64-Bit Version"
+fi
+
 # Get Directory
 SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 
-BUILDDIR=$SCRIPTPATH"build/"
+BUILDDIR=$SCRIPTPATH"/build/"
 rm -fr $BUILDDIR
 mkdir $BUILDDIR
 
-MAKEFL=$SCRIPTPATH"/Makefile"
-echo $MAKEFL
-OBJS=""
+cd "$BUILDDIR"
+pwd
 
-echo "" > $MAKEFL
-echo "CC=$GCCROOT/gcc -Wall -O3 " >> $MAKEFL
-echo "CP=$GCCROOT/g++ -Wall -O3 -w -std=c++0x  " >> $MAKEFL
-echo "LD=$GCCROOT/gcc -ldl -lpthread -lpng -D_GLIBCXX_USE_NANOSLEEP " >> $MAKEFL
-
-echo "Building Make File..."
-
-# Build C Files
-
-for file in $(find . -name '*.c'); do
-	echo "C FILE: $file"
-	echo "$BUILDDIR/$file.o : $SCRIPTPATH$file" >> $MAKEFL
-	echo "	$""CC -c -o BUILDDIR/$file.o $SCRIPTPATH$file" >> $MAKEFL
-	echo "" >> $MAKEFL
-	OBJS=$OBJS" "$BUILDDIR"/"$file".o"
-done
-
-for file in $(find . -name '*.cpp'); do
-	echo "CXX FILE: $file"
-	echo "$BUILDDIR/$file.o : $SCRIPTPATH$file" >> $MAKEFL
-	echo "	$""CP -c -o BUILDDIR/$file.o $SCRIPTPATH$file" >> $MAKEFL
-	echo "" >> $MAKEFL
-	OBJS=$OBJS" "$BUILDDIR"/"$file".o"
-done
-
-echo "all : $OBJS" >> $MAKEFL
-echo "	$""LD -o fractServ $OBJS" >> $MAKEFL
+echo "Calling CMake..."
 
 
-OUTDIR=""
+cmake -DEXFLAG=$FLAG -DCMAKE_CXX_COMPILER=$GCCROOT/g++ -DCMAKE_CC_COMPILER=$GCCROOT/gcc -DCMAKE_LINKER=$GCCROOT/gcc ../
 
-# first 
 
+echo "Ready To Build..."
+
+make
