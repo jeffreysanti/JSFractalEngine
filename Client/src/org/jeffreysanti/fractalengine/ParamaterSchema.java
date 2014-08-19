b2044f0c2d3d26c82a9b0d8c28716a0a772a23a0
Jeffrey Santi <jeffreysanti@gmail.com>
2014-08-19 12:14:38 -0400

    Moved schema to java resource

--- Client/src/org/jeffreysanti/fractalengine/ParamaterSchema.java
+++ Client/src/org/jeffreysanti/fractalengine/ParamaterSchema.java
@@ -7,6 +7,9 @@ package org.jeffreysanti.fractalengine;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
 import java.util.HashMap;
 
 /**
@@ -48,16 +51,10 @@ public class ParamaterSchema {
     
     ParamaterSchema(String schemaFile) {
         
-        // correct schema file name: Fix Windows Bug
-        String path = this.getClass().getProtectionDomain().getCodeSource().getLocation().getPath();
-        File f = new File(path);
-        path = f.getParent();
-        
-        schemaFile = path + "/" + schemaFile;
-        
         C = new HashMap();
         try{
-            BufferedReader br = new BufferedReader(new FileReader(schemaFile));
+            InputStream in = getClass().getResourceAsStream("/org/jeffreysanti/fractalengine/"+schemaFile);
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
             String line;
             while ((line = br.readLine()) != null) {
                if(line.length() < 2)
