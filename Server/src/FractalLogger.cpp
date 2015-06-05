6384425891550ad5a39fefa9b7b4ffbf88de9c37
Jeffrey Santi <jeffreysanti@gmail.com>
2015-06-04 21:10:58 -0400

    Attempt at logger not working outide dev environment

--- Server/src/FractalLogger.cpp
+++ Server/src/FractalLogger.cpp
@@ -61,11 +61,13 @@ void FractalLogger::write(int fid, std::string data)
 	else if(F[bucket].open){
 		F[bucket].file.close();
 		F[bucket].fid = fid;
-		F[bucket].file.open(std::string(concat(FractalGen::getSaveDir()+"/",fid) +".log").c_str(), std::ios::app);
+		F[bucket].file.open(std::string(concat(FractalGen::getSaveDir()+"/",fid) +".log").c_str(),
+				std::ios::out | std::ios::app);
 		F[bucket].file << data;
 		F[bucket].file.flush();
 	}else{
-		F[bucket].file.open(std::string(concat(FractalGen::getSaveDir()+"/",fid) +".log").c_str(), std::ios::app);
+		F[bucket].file.open(std::string(concat(FractalGen::getSaveDir()+"/",fid) +".log").c_str(),
+				std::ios::out | std::ios::app);
 		F[bucket].fid = fid;
 		F[bucket].open = true;
 		F[bucket].file << data;
